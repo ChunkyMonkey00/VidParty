acd679d16f4d3f7a35f23ee396711ef57444f8cf
ChunkyMonkey00 <148571454+ChunkyMonkey00@users.noreply.github.com>
2024-06-03 11:44:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function gel(id) {
   gel("hostButton").onclick = confirmHost;
   
   function confirmHost() {
-    
+    window.location = "https://chunkymonkey00.github.io/VidParty/host.html";
   }
